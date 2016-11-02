5cb5939091f3bb258bd9c022577f6e9b9209e83d
Programa de Ingeniería de Sistemas de la Universidad Francisco de Paula Santander <juancasilva@gmail.com>
2016-11-02 13:59:59 -0500

    Create html_finalSripts.php

---
+++ vistas/html_finalSripts.php
@@ -0,0 +1,13 @@
+<?php
+?>
+    <!--Algunos componentes requieren el uso de la librería en javascript-->
+    <script type="text/javascript" src="js/ufps.js"></script>
+    <script type="text/javascript" src="js/wpuniversity.js"></script>
+    <script type='text/javascript'>
+    /* <![CDATA[ */
+    var NIVO = {"effect":"sliceDown","pauseOnHover":"enable","controlNav":"disable","directionNav":"enable","directionNavHide":"disable","animSpeed":"500","pauseTime":"6000","captionOpacity":"0.75"};
+    /* ]]> */
+    </script>
+    <script type='text/javascript' src='js/jquery.nivo.slider.pack.js?ver=1.0'></script>
+<?php
+?>
