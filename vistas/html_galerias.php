6e48d7d236a82ea144240d6aa5df800649026eb9
Programa de Ingeniería de Sistemas de la Universidad Francisco de Paula Santander <juancasilva@gmail.com>
2016-11-02 14:00:52 -0500

    Create html_galerias.php

---
+++ vistas/html_galerias.php
@@ -0,0 +1,53 @@
+<?php
+?>
+            <div class="gdl-custom-sidebar">
+              <h3 class="gdl-custom-sidebar-title-m">Galer&iacute;as</h3>
+              <div class="klisting gridLayout">
+                <div id="latestTabContent" class="tabContent">
+                  <div class="yesFeaturedImg post-624 post type-post status-publish format-standard has-post-thumbnail hentry category-breakfast-and-brunch category-drinks category-recipes tag-all tag-asian tag-indian">
+                    <a class="featuredImage" href="">
+                    <img width="350" height="350" src="img/tnufps01.jpg" class="attachment-post-thumbnail size-post-thumbnail wp-post-image" alt="tea" />       </a>
+                    <div class="klistingContent">
+                      <h2 class="posttitle"><a href="" rel="bookmark">Participaci&oacute;n en un evento internacional</a></h2>
+                    </div><!--end listingContent-->
+                  </div><!--end post-->
+                  <div class="yesFeaturedImg post-631 post type-post status-publish format-standard has-post-thumbnail hentry category-desserts category-drinks category-featured category-recipes tag-all tag-chocolate">
+                    <a class="featuredImage" href="">
+                    <img width="350" height="350" src="img/tnufps02.jpg" class="attachment-post-thumbnail size-post-thumbnail wp-post-image" alt="Hot chocolate" />     </a>
+                    <div class="klistingContent">
+                      <h2 class="posttitle"><a href="" rel="bookmark">Actividad social en un sector espec&iacute;fico</a></h2>
+                    </div><!--end listingContent-->
+                  </div><!--end post-->
+                  <div class="yesFeaturedImg post-397 post type-post status-publish format-standard has-post-thumbnail hentry category-appetizers-and-snacks category-recipes tag-all tag-chicken tag-indian tag-meat tag-rice tag-spicy tag-vegetarian">
+                    <a class="featuredImage" href="">
+                    <img width="350" height="350" src="img/tnufps03.jpg" class="attachment-post-thumbnail size-post-thumbnail wp-post-image" alt="indian_platter" />    </a>
+                    <div class="klistingContent">
+                      <h2 class="posttitle"><a href="" rel="bookmark">Ganadores de un concurso de dise&ntilde;o</a></h2>
+                    </div><!--end listingContent-->
+                  </div><!--end post-->
+                  <div class="yesFeaturedImg post-308 post type-post status-publish format-standard has-post-thumbnail hentry category-appetizers-and-snacks category-recipes tag-all tag-asian tag-fish tag-japanese tag-seafood">
+                    <a class="featuredImage" href="">
+                    <img width="350" height="350" src="img/tnufps04.jpg" class="attachment-post-thumbnail size-post-thumbnail wp-post-image" alt="salad" />     </a>
+                    <div class="klistingContent">
+                      <h2 class="posttitle"><a href="" rel="bookmark">Curso de profundizaci&oacute;n</a></h2>
+                    </div><!--end listingContent-->
+                  </div><!--end post-->
+                  <div class="yesFeaturedImg post-317 post type-post status-publish format-standard has-post-thumbnail hentry category-desserts category-featured category-recipes tag-all tag-eggs tag-party">
+                    <a class="featuredImage" href="">
+                    <img width="350" height="350" src="img/tnufps05.jpg" class="attachment-post-thumbnail size-post-thumbnail wp-post-image" alt="ice_cream" /> </a>
+                    <div class="klistingContent">
+                      <h2 class="posttitle"><a href="" rel="bookmark">Estudiante de intercambio en Canad&aacute;</a></h2>
+                    </div><!--end listingContent-->
+                  </div><!--end post-->
+                  <div class="yesFeaturedImg post-314 post type-post status-publish format-standard has-post-thumbnail hentry category-breakfast-and-brunch category-desserts category-drinks category-recipes tag-all tag-coffee">
+                    <a class="featuredImage" href="">
+                    <img width="350" height="350" src="img/tnufps06.jpg" class="attachment-post-thumbnail size-post-thumbnail wp-post-image" alt="coffee" />    </a>
+                    <div class="klistingContent">
+                      <h2 class="posttitle"><a href="" rel="bookmark">Campus party 2015</a></h2>
+                    </div><!--end listingContent-->
+                  </div><!--end post-->
+                </div><!--end popularTabContent-->
+              </div><!--end listing-->
+            </div>
+<?php
+?>
