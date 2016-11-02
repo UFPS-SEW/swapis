4cbfdb9ec3e2fc24d542458fccf27c9bb5ba8b56
Programa de Ingeniería de Sistemas de la Universidad Francisco de Paula Santander <juancasilva@gmail.com>
2016-11-02 14:01:50 -0500

    Create html_informaciones.php

---
+++ vistas/html_informaciones.php
@@ -0,0 +1,14 @@
+<?php
+?>
+            <div class="gdl-custom-sidebar">
+              <h3 class="gdl-custom-sidebar-title-m">Informaciones</h3>
+            </div>
+            <div class="gdl-custom-sidebar" id="eventos">
+              <a onclick="filtreEvento(event, 'eventos', '0')" class="ufps-btn ufps-btn-light-active">Todas</a>
+              <a onclick="filtreEvento(event, 'eventos', '1')" class="ufps-btn ufps-btn-light">Noticias</a>
+              <a onclick="filtreEvento(event, 'eventos', '2')" class="ufps-btn ufps-btn-light">Anuncios</a>
+              <a onclick="filtreEvento(event, 'eventos', '3')" class="ufps-btn ufps-btn-light">Eventos</a>
+              <a onclick="filtreEvento(event, 'eventos', '4')" class="ufps-btn ufps-btn-light">Convocatorias</a>
+            </div>
+<?php
+?>
