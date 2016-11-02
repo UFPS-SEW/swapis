3183a929ed51771a14cc9c4a278f0afa11adb0f5
Programa de Ingeniería de Sistemas de la Universidad Francisco de Paula Santander <juancasilva@gmail.com>
2016-11-02 14:01:30 -0500

    Create html_header.php

---
+++ vistas/html_header.php
@@ -0,0 +1,29 @@
+<?php
+?>
+  <head>
+    <!--Metaetiqueta para el uso del conjunto de caracteres adecuado-->
+    <meta charset="utf-8">
+    <!--Metaetiqueta para corregir compatibilidad con navegador Microsft-->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!--Metaetiqueta para la correcta visualización en dispositivos moviles-->
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=0">
+    <title>Programa de Ingenieria de Sistemas UFPS</title>
+    <!--Añada primero el estilo de la libreria (ufps.css o ufps.min.css) y luego sus estilos propios-->
+    <link href="css/ufps.css" rel="stylesheet">
+    <link href="css/styles.css" rel="stylesheet">
+    <link href="css/wpuniversity.css" rel="stylesheet">
+    <link href="css/wpunivresp.css" rel="stylesheet">
+    <link rel='stylesheet' id='nivo-slider-css'  href='css/nivo-slider.css' type='text/css' media='all' />
+    <link rel='stylesheet' id='nivo-slider-style-css'  href='css/nivo-slider-style.css' type='text/css' media='all' />
+    <link rel='stylesheet' id='stylesheet-css'  href='css/wpkitchen.css' type='text/css' media='all' />
+    <script type='text/javascript' src='js/jquery/jquery.1.12.4.js'></script>
+    <script type='text/javascript' src='js/jquery/jquery-migrate.min.js'></script>
+    <link href="favicon.ico" rel="shortcut icon" type="image/vnd.microsoft.icon" />
+    <!--Librerías para compatibilidad con versiones antiguas de Internet Explorer-->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+<?php
+?>
